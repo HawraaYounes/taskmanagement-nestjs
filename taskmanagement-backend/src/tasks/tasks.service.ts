dc5e0eae28da7fa14d216bd220056026b1ee2790
HawraaYounes <hawraayounes0@gmail.com>
2023-03-08 11:42:23 +0200

    Adding validation getTaskById

--- taskmanagement-backend/src/tasks/tasks.service.ts
+++ taskmanagement-backend/src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -24,7 +24,11 @@ export class TasksService {
   }
 
   getTaskById(id:string):Task{
-    return this.tasks.find(task=>task.id===id);
+    const task=this.tasks.find(task=>task.id===id);
+    if(!task){
+      throw new NotFoundException(`${id} is Invalid ID.`)
+    }
+    return task;
   }
 
   createTask(createTaskDto:CreateTaskDto): Task {
