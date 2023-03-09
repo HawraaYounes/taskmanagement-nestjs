8e33bf1c83e75fc3e6314953f6d474df2d150903
HawraaYounes <hawraayounes0@gmail.com>
2023-03-09 07:10:42 +0200

    Applying repository pattern

--- taskmanagement-backend/src/app.module.ts
+++ taskmanagement-backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
+
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
